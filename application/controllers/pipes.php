2ee91a689a3581e82ec4a00b80262de34524aa8e
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-02-09 10:05:50 +0000

    Basic Pipes controller with a simple lookup

---
+++ application/controllers/pipes.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Pipes
+ *
+ * Sexy PHP package management
+ *
+ * @package pipes
+ * @subpackage server
+ * @author Jamie Rumbelow <http://jamierumbelow.net>
+ * @copyright Copyright (c) 2010 Jamie Rumbelow
+ * @license MIT License
+ **/
+
+class Pipes extends CI_Controller {
+	public $data = array();
+	
+	public function index() {
+		// Setup some vars
+		$this->data['pipes'] = array();
+		$this->data['search'] = $this->input->get('search');
+		
+		// Do a pipes lookup
+		$pipes = preg_grep("/".$this->data['search']."(.+)\.pipe/", scandir(FCPATH.'pipestore'));
+		
+		// Respond to both JSON and HTML
+		if (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] == 'application/json') {
+			$this->output->set_header('Content-Type: application/json');
+			$this->load->view('pipes/index.json.php', $this->data);
+		} else {
+			$this->load->view('pipes/index.html.php', $this->data);
+		}
+	}
+}
