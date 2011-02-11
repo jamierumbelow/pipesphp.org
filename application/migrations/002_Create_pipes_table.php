27cf92b95bbc5415c8adbb58984ede99ebab3487
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-02-11 20:31:16 +0000

    Adding the pipes and versions table migrations

---
+++ application/migrations/002_Create_pipes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+class Migration_create_pipes_table extends Migration {
+	public function up() {
+		$this->dbforge->add_field(array(
+			'id' 			=> array('type' => 'INT', 'auto_increment' => TRUE, 'unsigned' => TRUE),
+			'name'			=> array('type' => 'VARCHAR', 'constraint' => 160),
+			'slug'			=> array('type' => 'VARCHAR', 'constraint' => 160),
+			'description'	=> array('type' => 'TEXT'),
+			'downloads'		=> array('type' => 'INT'),
+			'created_at'	=> array('type' => 'DATETIME'),
+			'updated_at'	=> array('type' => 'DATETIME')
+		));
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('pipes');
+		
+		$this->dbforge->add_field(array(
+			'pipe_id' 		=> array('type' => 'INT'),
+			'pipespec'		=> array('type' => 'TEXT'),
+			'downloads'		=> array('type' => 'INT'),
+			'created_at'	=> array('type' => 'DATETIME')
+		));
+		$this->dbforge->create_table('versions');
+	}
+	
+	public function down() {
+		$this->dbforge->drop_table('pipes');
+		$this->dbforge->drop_table('versions');
+	}
+}
